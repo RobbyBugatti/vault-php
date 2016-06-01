a6172b8a35b9b00c9cccce4fcc884af5a423fc9e
Robert Wittman <rob@ihsdigital.com>
2016-06-01 13:52:38 -0400

    Initial Commit

---
+++ src/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vault;
+
+class Vault
+{
+    public function __construct()
+    {
+        error_log("Constructing our Vault");
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Vault\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
